cb2af7cfdb627138f645dd650a1a65fd1b87633f
julienroy13 <jul.roy1311@gmail.com>
2018-04-05 19:05:21 -0400

    Most of the environment works. Rendering does not work

--- env.py
+++ env.py
@@ -1,12 +1,12 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-#import scipy.signal
+import scipy.signal
 
 
 class Connect4(object):
 
-	def __init__(self, n_rows=6, n_columns=7, win_streak=4):
+	def __init__(self, n_rows=6, n_columns=7, win_streak=4, render=False):
 
 		self.n_rows = n_rows
 		self.n_columns = n_columns
@@ -14,14 +14,19 @@ class Connect4(object):
 		self.grid = np.zeros(shape=(n_rows, n_columns))
 
 		# Creates kernels to check the different winning conditions
-		h_win_kernel = np.zeros(shape=(1, win_streak)) # horizontal win
-		v_win_kernel = np.zeros(shape=(win_streak, 1)) # vertical win
+		h_win_kernel = np.ones(shape=(1, win_streak)) # horizontal win
+		v_win_kernel = np.ones(shape=(win_streak, 1)) # vertical win
 		d1_win_kernel = np.zeros(shape=(win_streak, win_streak)) # diagonal-1 win
 		d2_win_kernel = np.zeros(shape=(win_streak, win_streak)) # diagonal-2 win
 		for i in range(win_streak):
 			d1_win_kernel[i,i] = 1
 			d2_win_kernel[i, win_streak-i-1] = 1
 		self.win_kernels = [h_win_kernel, v_win_kernel, d1_win_kernel, d2_win_kernel]
+
+		if render:
+			self.fig, self.ax = plt.subplots(1, 1, figsize=(8, 8))
+			self.plot = self.ax.imshow(self.grid)
+			self.update_plot()
 
 	def make_move(self, player_id, column, imaginary=False):
 		"""Places a piece of the player's color in the given column"""
@@ -56,23 +61,23 @@ class Connect4(object):
 				valid_columns.append(column)
 
 		return valid_columns
-	"""
+	
 	def check_win(self, player_id):
-		#Checks if the provided player has won the game
+		"""Checks if the provided player has won the game"""
 
 		# Only keeps the position of the player's pieces we are concerned with
 		player_pieces = (self.grid == player_id)
 		for kernel in self.win_kernels:
 
 			# Convolves the grid with the wining condition kernels
-			win_mask = scipy.signal.convolve2d(self.grid, kernel, mode="full")
+			win_mask = scipy.signal.convolve2d(player_pieces, kernel, mode="full")
 			has_won = np.any(win_mask >= self.win_streak)
 			
 			if has_won:
+				print("Kernel that got activated : ", kernel)
 				break
 
 		return has_won
-	"""
 
 	def reset(self):
 		self.grid = np.zeros(shape=(self.n_rows, self.n_columns))
@@ -80,11 +85,9 @@ class Connect4(object):
 	def print_grid(self):
 		print(self.grid, "\n")
 
-	def show_grid(self):
-		self.fig, self.ax = plt.subplots(1, 1, figsize=(4, 2))
-		plt.figure(figsize=(8,8))
-		plt.imshow(self.grid)
-		plt.show()
+	def update_plot(self):
+		self.plot.set_data(self.grid)
+		plt.pause(0.1)
 
 		"""
 		inset = u // 8
@@ -106,7 +109,7 @@ class Connect4(object):
 
 if __name__ == "__main__":
 
-	game = Connect4()
+	game = Connect4(render=True)
 
 	while 1:
 
@@ -116,24 +119,22 @@ if __name__ == "__main__":
 		a = int(input("Your move : "))
 		game.make_move(1, a)
 		game.print_grid()
+		game.update_plot()
 
-		"""
 		if game.check_win(1):
 			print("Congratulations! You have won!")
 			break
-		"""
 
 		# Random move from opponent
 		A_s = game.get_valid_moves()
 		a_id = np.random.randint(len(A_s))
 		game.make_move(2, A_s[a_id])
 		game.print_grid()
+		game.update_plot()
 
-		"""
 		if game.check_win(2):
 			print("Player 2 have won. You got beaten by a random bot...")
 			break
-		"""
 
 	print("GAME OVER")
 
