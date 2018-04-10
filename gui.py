0fb415fc9d600f6987549c414b98e1d4e11cea58
julienroy13 <jul.roy1311@gmail.com>
2018-04-10 17:50:31 -0400

    GUI now works like a charm for human players!

--- gui.py
+++ gui.py
@@ -44,26 +44,27 @@ class GuiManager(QtWidgets.QWidget):
         self.ResetBtn = QtGui.QPushButton("Reset")
         sizePolicyBtn.setHeightForWidth(self.ResetBtn.sizePolicy().hasHeightForWidth())
         self.ResetBtn.setSizePolicy(sizePolicyBtn)
-        self.ResetBtn.setStyleSheet("background-color: gray; color: black; font-size: 32px; font: bold; border-radius: 3px")
+        self.ResetBtn.setStyleSheet("background-color: gray; color: black; font-size: 24px; font: bold; border-radius: 3px")
         self.ResetBtn.clicked.connect(self.reset)
         self.ResetBtn.setEnabled(True)
 
         # GraphicView
-        self.Image = pg.ImageItem()
+        self.Image = pg.ImageItem(autoLevels=False)
         self.Image.mousePressEvent = self.add_piece
         self.Plot = pg.PlotWidget(title="Connect4", labels={"left":"Rows", "bottom":"Columns"})
         self.Plot.hideAxis("bottom")
         self.Plot.hideAxis("left")
         self.Plot.addItem(self.Image)
-        self.coeff=10 # For padding the image
+        self.coeff=21 # For padding the image (has to be odd)
 
         # Build lookup table
         lut = np.zeros((256,3), dtype=np.ubyte)
         lut[0,:] = 50
-        lut[1,0] = 150
-        lut[2:,0] = 255
-        lut[:,1] = np.arange(0,256)
+        lut[1,:] = [255, 0, 0]
+        lut[2:,:] = [0, 0, 255]
+        lut[255,:] = 255
         self.Image.setLookupTable(lut, update=True)
+
 
         # Instanciates the layout
         self.layout = QtWidgets.QGridLayout()
@@ -81,7 +82,38 @@ class GuiManager(QtWidgets.QWidget):
         self.show()
         self.updatePlot()
 
+    def reset(self):
+        self.game.reset()
+        self.updatePlot()
+    
+    def add_piece(self, event):
+        if not(self.game.over):
+            # Tries to add a piece (if the column isn't full)
+            try:
+                row = self.getPos(event)
+                self.game.make_move(self.game.turn, row)
+                self.game.check_win(1)
+                self.game.check_win(2)
+                self.updatePlot()
+            except InvalidMove as e:
+                print(e)
+
+    def getPos(self , event):
+        row = int(event.pos().x())
+        row = int(row / self.coeff)
+        
+        return row
+
+    def updatePlot(self):
+        # Updates the plot with the current state of the game
+        pretty_grid = self.pad_grid(self.game.grid, coeff=self.coeff)
+        self.Image.setImage(image=np.transpose(np.flip(pretty_grid, axis=0)))
+        self.Image.setLevels([0, 255], update=True)
+        return
+
     def pad_grid(self, grid, coeff):
+        
+        # Put some padding between each element (cause you know.. pretty)
         grid = np.repeat(grid, coeff, axis=0)
         grid = np.repeat(grid, coeff, axis=1)
         
@@ -90,36 +122,38 @@ class GuiManager(QtWidgets.QWidget):
         mask_j = np.invert((mask_j % coeff == 0) + (mask_j % coeff == coeff-1)).astype(np.int)
         
         grid = grid * mask_i * mask_j
+
+        # Final padding to make the sides pretty
         grid = np.pad(grid, pad_width=1, mode="constant")
+
+        # Highlights the winning line (just for prettiness)
+        if self.game.over:
+            grid = self.highlight_win_line(grid, self.game.win_indices)
         
         return grid
 
-    def updatePlot(self):
-        # Updates the plot with the current state of the game
-        pretty_grid = self.pad_grid(self.game.grid, coeff=self.coeff)
-        self.Image.setImage(image=np.transpose(np.flip(pretty_grid, axis=0)))
-        return
+    def highlight_win_line(self, grid, win_indices):
 
-    def reset(self):
-        self.game.reset()
-        self.updatePlot()
+        # We draw a white line around each winning piece
+        for i, j in win_indices:
+            new_i = i*self.coeff + (self.coeff//2) + 1
+            new_j = j*self.coeff + (self.coeff//2) + 1
 
-    
-    def add_piece(self, event):
-        # Tries to add a piece (if the column isn't full)
-        try:
-            self.getPos(event)
-        except InvalidMove as e:
-            print(e)
+            begin_i = new_i - (self.coeff//2) - 1
+            end_i = new_i + (self.coeff//2) + 1
+            begin_j = new_j - (self.coeff//2) - 1
+            end_j = new_j + (self.coeff//2) + 1
 
-    def getPos(self , event):
-        row = int(event.pos().x())
-        row = int(row / 10)
-        print(row)
-        
-        self.game.make_move(self.game.turn, row)
-        self.updatePlot()
-        return
+            # Draws vertical lines
+            grid[begin_i : end_i+1, [begin_j, begin_j+1, end_j, end_j-1]] = 255
+
+            # Draws horizontal lines
+            grid[[begin_i, begin_i+1, end_i, end_i-1], begin_j : end_j+1] = 255
+
+        return grid
+
+
+
 
 
 # If we run the file directly
